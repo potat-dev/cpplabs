c7939f2f52bc62bfd847929e62d86a2a4b4bbf6b
DenCoder618 <denis100703@gmail.com>
2021-09-17 21:49:06 +0300

    lab

---
+++ lab0-n7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Ввести три целых числа
+// Вычислить и вывести произведение первого и второго числа
+
+int main()
+{
+    int a, b, c;
+    scanf("%d" "%d" "%d", &a, &b, &c);
+    printf("%d", a * b);
+    return 0;
+}
