6e2a3b3e248fec670a88efdb44b9a8f25378081c
DenCoder618 <denis100703@gmail.com>
2021-09-22 01:07:15 +0300

    first working version of lab 2

--- Lab_2/Lab_2.c
+++ Lab_2/Lab_2.c
@@ -2,34 +2,94 @@
 // Можно считать, что все слова имеют длину ровно 3 символа
 
 #include <stdio.h>
+#include <string.h>
 
 #define WORDS_LIMIT 50
 #define WORDS_LEN 3
 #define STR_LIMIT (WORDS_LEN + 1) * WORDS_LIMIT
 
-int is_char(int c) {
-  return ((65 <= c && c <= 90) || (97 <= c && c <= 122)
-     || (128 <= c && c <= 175) || (224 <= c && c <= 239)) ? 1 : 0;
-}
+// int is_char(int c) {
+//   return ((65 <= c && c <= 90) || (97 <= c && c <= 122)
+//      || (128 <= c && c <= 175) || (224 <= c && c <= 239)) ? 1 : 0;
+// }
+
+// int strlen(char s[]) {
+//    int c = 0;
+//    while (s[c] != '\0') c++;
+//    return c;
+// }
 
 void main() {
+  char words[WORDS_LIMIT][WORDS_LEN + 1];
   char str[STR_LIMIT];
-  char c;
-  int i = 0;
+  
+  fgets(str, STR_LIMIT, stdin); // считываем строку вместе с пробелами --- работает!
 
-  // считываем строку вместе с пробелами --- работает!
-  fgets(str, STR_LIMIT, stdin);
-  // printf("%s", str);
+  int word = 0, k = 0;
+  for (int i = 0; i <= strlen(str); i++) {
+    if (str[i] == ' ' || str[i] == '\0') {
+      words[word][k] = '\0';
+      word++;
+      k = 0;
+    }  else {
+      words[word][k] = str[i];
+      k++;
+    }
+  }
 
-  do {
-    do {
-      c = str[i++];
-      if (is_char(c)) printf("%c - %d\n", c, (int)c);
-    } while (is_char(c) && (c != ' '));
-    if (c != '\0') printf("-- new word --\n");
-  } while (c != '\0');
+  printf("Dublicates:\n");
 
-  // сплиттим по " " --- работает!
-  // создаем массив слов
-  // проверяем повторы
+  for (int w = 0; w <= word; w++) {
+    int count = 1;
+
+    for (int i = 0; i <= word; i++) {
+      if (strcmp(words[i], words[w]) == 0 && strcmp(words[i], "\0") != 0 && w != i) {
+        strcpy(words[i],"\0");
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      printf("%s - %d times\n", words[w], count);
+    }
+  }
 }
+
+// char dublicates[WORDS_LIMIT][WORDS_LEN + 1](char** arr) {
+
+// }
+
+// void main() {
+//   char words[5][WORDS_LEN + 1];
+//   int n = 5;
+
+//   for (int i = 0; i < n; i++) {
+//     scanf("%03c", words[i]);
+//     words[i][4] = '\0';
+//   }
+
+//   for (int i = 0; i < n; i++) {
+//     printf("%s\n", words[i]);
+//   } 
+// }
+
+// int main()
+// {
+//     int i;
+  
+//     // Lets say we have 3 words
+//     int n = 3;
+  
+//     // Declaration of 2-D char array
+//     char array[n][20];
+  
+//     // Initialization of 2-D char array
+//     for (i = 0; i < 3; i++)
+//         scanf("%s", array[i]);
+  
+//     // print the words
+//     for (i = 0; i < 3; i++)
+//         printf("%s\n", array[i]);
+  
+//     return 0;
+// }
