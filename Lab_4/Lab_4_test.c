6969ecb6b38f5ebc92cb7c932f86ab045e4414f2
DenCoder618 <denis100703@gmail.com>
2021-09-26 05:06:02 +0300

    issue #7 - trying to implement dynamic arrays

---
+++ Lab_4/Lab_4_test.c
@@ -0,0 +1,86 @@
+// Ввести строку.
+// Вывести различные слова вместе с количеством их появления в строке
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WORDS_LIMIT 50
+#define WORDS_LEN 30
+#define STR_LIMIT (WORDS_LEN + 1) * WORDS_LIMIT
+
+#define strlen(x) (sizeof(x) / sizeof((x)[0]))
+
+int is_char(char c) {
+    return ((65<=c && c<=90)
+        ||  (97<=c && c<=122)
+        || (128<=c && c<=175)
+        || (224<=c && c<=239)) ? 1 : 0; }
+
+int strcmp(char s1[], char s2[]) { // аналог функции strcmp в string.h
+  for (int i = 0; ; i++) {
+    if (s1[i] != s2[i]) return 1; // строки не совпадают
+    if (s1[i] == '\0')  return 0; // строки совпадают
+} }
+
+void strcopy(char *dest, const char *src) { // аналог функции strcpy в string.h
+  char *p = dest;
+  do *p++ = *src; // колпируем каждый символ из src в dest
+  while (*src++);
+}
+
+// посимвольное получение динамической строки с пробелами c получением её реальной длины
+char *get_string(int *len) {
+    *len = 0;                               // изначально строка пуста
+    int capacity = 1;                       // ёмкость контейнера динамической строки (1, так как точно будет '\0')
+    char *s = (char *)malloc(sizeof(char)); // динамическая пустая строка
+    char c = getchar(); // символ для чтения данных
+    // читаем символы, пока не получим символ переноса строки (\n)
+    while (c != '\n') {
+        s[(*len)++] = c; // заносим в строку новый символ
+        // если реальный размер больше размера контейнера, то увеличим его размер
+        if (*len >= capacity) {
+            capacity *= 2;                                   // увеличиваем ёмкость строки в два раза
+            s = (char *)realloc(s, capacity * sizeof(char)); // создаём новую строку с увеличенной ёмкостью
+        }
+        c = getchar(); // считываем следующий символ
+    }
+    s[*len] = '\0'; // завершаем строку символом конца строки
+    return s; // возвращаем указатель на считанную строку
+}
+
+void main() {
+  int len = 0;
+  printf("Enter a string:\n");
+  char *str = get_string(&len); // считываем динамическую строку
+  printf("You've wrote: '%s', symbols: %d\n", str, len); // выводим строку и её длину
+  char words[WORDS_LIMIT][WORDS_LEN + 1] = {0};
+//   char str[STR_LIMIT] = {0};
+
+//   fgets(str, STR_LIMIT, stdin);
+  printf("\nWords:\n--------\n");
+
+  int word = 0, k = 0;
+  for (int i = 0; i <= strlen(str); i++) {
+    if (str[i] == ' ' || str[i] == '\0' || str[i] == '\n') {
+      words[word][k] = '\0';
+      if (str[i] != '\n') {
+        word++;
+      } else { break; }
+      k = 0;
+    } else if (is_char(str[i])) {
+      words[word][k] = str[i];
+      k++;
+  } }
+
+  int flag = 0;
+  for (int w = 0; w <= word + 1; w++) {
+    int count = 1;
+
+    for (int i = 0; i <= word + 1; i++) {
+      if (strcmp(words[i], words[w]) == 0 && w != i && strcmp(words[i], "\0") != 0) {
+        strcopy(words[i], "\0");
+        count++; } }
+    if (strcmp(words[w], "\0") != 0) printf("%s - %d times\n", words[w], count);
+  }
+  free(str); // освобождаем динамическую память
+}
