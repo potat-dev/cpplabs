a20fbc19d607c1a6e28664114ec24554c7a73121
Den <denis100703@gmail.com>
2021-10-03 04:03:25 +0300

    lab 5 working

--- Lab_5/Lab_5.c
+++ Lab_5/Lab_5.c
@@ -9,25 +9,44 @@
 int len = 0;
 
 char *readstr() {
-    len = 0;
-    int capacity = 1; // емкость динамической строки
-    char *s = (char*)malloc(sizeof(char)); // динамическая пустая строка
+  len = 0;
+  int capacity = 1;                       // емкость динамической строки
+  char *s = (char *)malloc(sizeof(char)); // динамическая пустая строка
+  char c = getchar();
+  while (c != '\n') {
+    s[(len)++] = c;
+    if (len >= capacity) {                             // если реальный размер больше размера контейнера, то увеличим его размер
+      capacity *= 2;                                   // увеличиваем емкость в два раза
+      s = (char *)realloc(s, capacity * sizeof(char)); // создаём новую строку с увеличенной емкостью
+    } c = getchar(); }
+  s[len] = '\0';
+  return s; // возвращаем указатель
+}
 
-    char c = getchar();
-    while (c != '\n') {
-        s[(len)++] = c;
-        if (len >= capacity) { // если реальный размер больше размера контейнера, то увеличим его размер
-            capacity *= 2;     // увеличиваем емкость в два раза
-            s = (char*)realloc(s, capacity*sizeof(char)); // создаём новую строку с увеличенной емкостью  
-        }
-        c = getchar();          
-    }
-    s[len] = '\0';
-    return s; // возвращаем указатель
+void print_char(char ch) {
+  switch (ch) {
+  case '.':
+    printf("DOT");
+    break;
+  case ',':
+    printf("COMMA");
+    break;
+  case '?':
+    printf("QUESTION");
+    break;
+  case ':':
+    printf("COLON");
+    break;
+  default:
+    printf("%c", ch);
+    break;
+  }
 }
 
 void main() {
-    char *str = readstr(); // считываем динамическую строку
-    printf("You've wrote: '%s', symbols: %d\n", str, len);
-    free(str);
+  printf("Input string:\n");
+  char *str = readstr(); // считываем динамическую строку
+  printf("\nString after replace:\n");
+  for (int i = 0; i < len; i++) print_char(str[i]);
+  free(str);
 }
