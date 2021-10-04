ec3b4bd6e1f3de0762cd7fe86634f4653597b003
Den <denis100703@gmail.com>
2021-10-05 02:51:58 +0300

    memory use reduced

--- Lab_6/Lab_6.c
+++ Lab_6/Lab_6.c
@@ -29,9 +29,18 @@ void main() {
 
   if ((f = fopen("input.txt", "r")) == NULL)
     { printf("Error! opening file"); exit(1); }
-  while (fscanf(f, "%s", words[words_count]) == 1) words_count++;
 
-  // counting words
+  while (fscanf(f, "%s", word) == 1) {
+    int flag = 0;
+    for (int i = 0; i < words_count; i++)
+      if (strcmp(word, words[i]) == 0) {
+        flag = 1;
+        break;
+      }
+    if (!flag) strcopy(words[words_count++], word);
+  }
+
+  // counting vowels
   int curr_word = 0;
   int counts[words_count];
   for (int w = 0; w < words_count; w++) {
