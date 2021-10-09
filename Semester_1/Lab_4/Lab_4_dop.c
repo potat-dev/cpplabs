b2722169fc695c740bb40d5fad616cd340d93c08
Den <denis100703@gmail.com>
2021-10-09 11:22:00 +0300

    lab 4 dop & lab 5 passed

--- Semester_1/Lab_4/Lab_4_dop.c
+++ Semester_1/Lab_4/Lab_4_dop.c
@@ -13,7 +13,9 @@
 
 int strcmp(const char *s1, const char *s2) {
   unsigned char c1, c2;
-  while ((c1 = *s1++) == (c2 = *s2++)) if (c1 == '\0') return 0;
+  while ((c1 = *s1++) == (c2 = *s2++))
+    if (c1 == '\0')
+      return 0;
   return c1 - c2;
 }
 
