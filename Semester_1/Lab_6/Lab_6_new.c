30c0e8fc4566c92c21e8ec89a5d82859ab3e0b25
Den <denis100703@gmail.com>
2021-10-13 01:35:04 +0300

    issue #14 fix - sorting now working

--- Semester_1/Lab_6/Lab_6_new.c
+++ Semester_1/Lab_6/Lab_6_new.c
@@ -3,22 +3,58 @@
 // подключаем файл с функциями
 #include "snippets.c"
 
-void sort_list(list *list, int sort_order) {
+int check_alphabet(char *w1, char *w2, int size)
+{
+  char c1, c2;
+  // printf("  (%s %s) ", w1, w2);
+    for (int i = 0; i < size; i++)
+    {
+      if (w1[i] < w2[i]) {
+        return 1;
+      } else if (w1[i] > w2[i]) {
+        return 0;
+      }
+    }
+    return 0;
+
+  // char c1, c2;
+  // while ((c1 == *w1++) == (c2 = *w2++))
+  //   if (c1 == '\0')
+  //     return 0;
+  // return 1;
+}
+
+void sort_list(list *list) { // int sort_order
 	node *curr = list->head;
-	for (size_t i = 0; i < list->size; i++) {
-    for (size_t j = i + 1; j < list->size; j++) {
+	// for (size_t i = 0; i < list->size; i++) {
+    for (size_t j = 0; j < list->size; j++) { /// спросить Янковского за size_t
+    // for (size_t j = i + 1; j < list->size; j++) {
+    for (size_t i = (list->size - 1); i > j; i--) {
       struct node *node1 = get_node(list, i);
-      struct node *node2 = get_node(list, j);      
-      if (sort_order * strcmp(node1->word->arr, node2->word->arr) > 0) {
+      struct node *node2 = get_node(list, i-1);
+      int min_node = node1->word->size < node2->word->size ? node1->word->size : node2->word->size;
+      // int need_to_sort = check_alphabet(&(node1)->word->arr, &(node2)->word->arr, min_node, 0);
+      int need_to_sort = check_alphabet(node1->word->arr, node2->word->arr, min_node);
+      // printf("%s %s - %d\n", node1->word->arr, node2->word->arr, need_to_sort);
+
+      // // printf("%s %s - %d %d - %d - %d\n------------------\n\n",
+      // node1->word->arr, node2->word->arr,
+      // node1->word->size, node2->word->size, min_node,
+      // // need_to_sort);
+
+      if (need_to_sort) {
+      // if (strcmp((&node1->word->arr), (&node2->word->arr)) > 0) {
         swap_nodes(node1, node2);
       }
     }
   }
 }
 
+
+
 void main() {
   FILE *file;
-  if ((file = fopen("input_small.txt", "r")) == NULL) {
+  if ((file = fopen("input.txt", "r")) == NULL) {
     printf("Error opening file!");
 		exit(1);
   }
@@ -37,7 +73,20 @@ void main() {
   init(&words);
   parse_str(str, file_size, &words);
 
-  sort_list(&words, 1);
+  // struct node *node1 = get_node(&words, 2);
+  // struct node *node2 = get_node(&words, 3); 
+  // size_t min_node = node1->word->size < node2->word->size ? node1->word->size : node2->word->size;
+  // // printf("%d %d %d\n", node1->word->size,node2->word->size, min_node); 
+  // printf("%d\n\n", check_alphabet(&(node1)->word->arr, &(node2)->word->arr, min_node, 1));
+  
+  
+  sort_list(&words);
+  
+  // printf("%d\n\n", check_alphabet(&(node1)->word->arr, &(node2)->word->arr, min_node, 0));
+  
+  // if () {
+  //   swap_nodes(node1, node2);
+  // }
 
   print_list(&words);
   destroy(&words);
@@ -47,10 +96,12 @@ void main() {
 
   // swap_nodes(get_node(&words, 0), get_node(&words, 1));
 
-  // struct node *node1 = get_node(&words, 0);
-  // struct node *node2 = get_node(&words, 1);      
+      
   // if (strcmp(&(node1)->word->arr, &(node2)->word->arr) > 0) {
   //   swap_nodes(node1, node2);
   // }
 
-  // printf("%d\n\n", strcmp(&(node1)->word->arr, &(node2)->word->arr));
+ 
+
+
+  
