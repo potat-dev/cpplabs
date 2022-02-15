32fbdc0ae4c21bfd727a3d24a50fc52390d61654
Den <denis100703@gmail.com>
2022-02-16 01:59:02 +0300

    polynomial multiplication is workinggit add .

--- Semester_2/Lab_9/main.cpp
+++ Semester_2/Lab_9/main.cpp
@@ -6,6 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// как хранится 3x^2 + 4x + 5:
+//   {5, 4, 3}
+// i: 0, 1, 2
+// => x^i
+// а при умножении: x^(a+b)
+
 struct polynom {
   int n; // - количество коэффициентов
   int *arr; // массив коэффициентов
@@ -23,37 +29,52 @@ void destroy(polynom *p) {
   free(p);
 }
 
-polynom *multiply(polynom *a, polynom *b) {
-  printf("polynom 1: ");
-  for (int i = 0; i < a->n; i++)
-    printf(i+1 == a->n ? "%d" : "%d, ", a->arr[i]);
-
-  printf("\npolynom 2: ");
-  for (int i = 0; i < b->n; i++)
-    printf(i+1 == b->n ? "%d" : "%d, ", b->arr[i]);
-  
+void print_polynom(polynom *p) {
+  for (int i = p->n - 1; i >= 0; i--) {
+    if (p->arr[i]) {
+      if (i < 2) {
+        printf(i ? "%dx + " : "%d", p->arr[i]);
+      } else {
+        printf("%dx^%d + ", p->arr[i], i);
+      }
+    }
+  }
   printf("\n");
-  return a;
 }
 
-// как хранится 3x^2 + 4x + 5:
-//   {5, 4, 3}
-// i: 0, 1, 2
-// => x^i
-// а при умножении: x^(a+b)
+polynom *multiply(polynom *a, polynom *b) {
+  int max_power = a->n + b->n - 1;
+  int *arr = (int*)malloc(max_power * sizeof(int));
+  for (int i = 0; i < max_power; i++) arr[i] = 0;
+
+  for (int x = 0; x < a->n; x++) {
+    for (int y = 0; y < b->n; y++) {
+      arr[x+y] += a->arr[x] * b->arr[y];
+    }
+  }
+  // for (int i = 0; i < max_power; i++) printf("%d ", arr[i]);
+  // printf("\n");
+  polynom *temp = new_polynom(arr, max_power);
+  return temp;
+}
 
 int main() {
-  int arr1[3] = {1, 2, 3};
-  int arr2[4] = {6, 1, 8};
+  int arr1[3] = {1, 45, 3};
+  int arr2[4] = {1, 6, 1, 8};
 
   polynom *p1 = new_polynom(arr1, 3);
   polynom *p2 = new_polynom(arr2, 4);
 
-  int n = 0;
-  printf("input n = ");
-  scanf("%d", &n);
+  print_polynom(p1);
+  print_polynom(p2);
 
-  for (int i = 0; i < n; i++) {
-    multiply(p1, p2);
-  }
+  polynom *m = multiply(p1, p2);
+  print_polynom(m);
+
+  // int n = 0;
+  // printf("input n = ");
+  // scanf("%d", &n);
+  // for (int i = 0; i < n; i++) {
+  //   multiply(p1, p2);
+  // }
 }
