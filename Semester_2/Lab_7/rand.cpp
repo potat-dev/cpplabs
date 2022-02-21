fbe4a76d1aaddd4cb78a9ed704b101d2c42ea6e5
Den <denis100703@gmail.com>
2022-02-21 20:07:06 +0300

    Lab 7 passed

--- Semester_2/Lab_7/rand.cpp
+++ Semester_2/Lab_7/rand.cpp
@@ -4,11 +4,3 @@ int gen() {
   f0 = f1; f1 = f2; f2 = f;
   return f;
 }
-
-// int gen() {
-//   static int f1 = 1, f2 = 1, f3 = 0; 
-//   f3 = (f2 + f1) % 211;
-//   f1 = f2;
-//   f2 = f3;
-//   return f3;
-// }
