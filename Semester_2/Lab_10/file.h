185c67ad9ed7523d7820a7517882240334614251
Den <denis100703@gmail.com>
2022-03-03 04:03:25 +0300

    Lab 10 working

--- Semester_2/Lab_10/file.h
+++ Semester_2/Lab_10/file.h
@@ -6,3 +6,6 @@
 using namespace std;
 
 string read_file(const char *path);
+void read_file(const char *path, string &str);
+void split_lines(string &str);
+void write_file(string str, const char *path);
