7a896632b441c0b4aec0dbe81525720af96dfc36
Den <denis100703@gmail.com>
2022-03-03 05:27:12 +0300

    Lab 10 ready

---
+++ Semester_2/Lab_10/file.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "file.h"
+
+// форматируем файл с отступами
+void parse_file(const char *in, const char *out, int start, int end) {
+  FILE *input = fopen(in, "rt");
+  FILE *output = fopen(out, "w");
+  if (input == NULL) return;
+
+  int counter = 1;
+  char c = fgetc(input);
+  while (c != EOF) {
+    while (counter < start) {
+      fprintf(output, " ");
+      counter++;
+    }
+
+    while (counter == start && c == ' ')
+      c = fgetc(input);
+
+    if (counter <= end) {
+      fprintf(output, "%c", c);
+      c = fgetc(input);
+      counter++;
+    } else {
+      fprintf(output, "\n");
+      counter = 1;
+    }
+  }
+
+  fclose(input);
+  fclose(output);
+}
+
+// выводим файл
+void print_file(const char *path) {
+  FILE *file = fopen(path, "rt");
+  if (file == NULL) {
+    printf("failed to open file\n");
+    return;
+  }
+
+  printf("\n");
+  char c = fgetc(file);
+  while (c != EOF) {
+    printf("%c", c);
+    c = fgetc(file);
+  }
+
+  printf("\n");
+  fclose(file);
+}
---
+++ Semester_2/Lab_10/file.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void parse_file(const char *in, const char *out, int start, int end);
+void print_file(const char *path);
