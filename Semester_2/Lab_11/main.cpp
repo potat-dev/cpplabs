a4d19b35f73ed2ee4af562825d17a5e6ded78984
Den <denis100703@gmail.com>
2022-03-12 18:34:00 +0300

    strpbrk test program enhancement

--- Semester_2/Lab_11/main.cpp
+++ Semester_2/Lab_11/main.cpp
@@ -1,6 +1,34 @@
+// Написать функцию
+// const char* strpbrk(const char *string, const char *strCharSet)
+// работающую так же, как библиотечная и тестирующую программу к ней
+
 #include "strpbrk.h"
+#include <stdlib.h>
+
+int len = 0;
+char *readstr() {
+  len = 0;
+  int capacity = 1;
+  char *s = (char *)malloc(sizeof(char));
+  char c = getchar();
+  while (c != '\n') {
+    s[(len)++] = c;
+    if (len >= capacity) {
+      capacity *= 2;
+      s = (char *)realloc(s, capacity * sizeof(char));
+    }
+    c = getchar();
+  }
+  s[len] = 0;
+  return s;
+}
 
 int main() {
-  char *str = (char*) "test astr";
-  printf("%c", *strpbrk(str, "afe"));
+  printf("Enter string: ");
+  char *str = readstr();
+
+  printf("Enter allowed chars: ");
+  char *chrs = readstr();
+  
+  printf("The first character found: %c", *strpbrk(str, chrs));
 }
