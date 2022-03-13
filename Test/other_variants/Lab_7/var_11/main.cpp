e9b3ddf8fee8ddc1568ba66a91615b48e0858e66
Den <denis100703@gmail.com>
2022-03-13 22:45:37 +0300

    normal_frand() implemented

--- Test/other_variants/Lab_7/var_11/main.cpp
+++ Test/other_variants/Lab_7/var_11/main.cpp
@@ -5,14 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 5
+#define NUM 500000
+#define POWER 12
+
 #define frand() ((float)(rand())/(RAND_MAX))
 
-float frand5() {
-  return frand() * 10 - 5;
+// [-n ... n]
+float frand_n(int n) {
+  return frand() * n * 2 - n;
+}
+
+// [-n ... n] норм. распределение
+// Центральная Предельная теорема
+float normal_frand_n(int count, int n) {
+  float sum = 0;
+  for (int i = 0; i < count; i++)
+    sum += frand_n(n);
+  return sum / count;
+}
+
+// округление: i <= f < i+1
+int round(float f) { 
+  return (int)(f > 0 ? f : f - 1);
 }
 
 int main() {
-  for (int i = 0; i < 20; i++) {
-    printf("%f\n", frand5());
-  }
+  int freq[N * 2] = {0};
+  for (int i = 0; i < NUM; i++)
+    freq[round(normal_frand_n(POWER, N)) + N]++;
+
+  for (int i = 0; i < N * 2; i++)
+    printf("(%2d, %2d) - %d\n", i-N, i-N+1, freq[i]);
 }
