de15fd42c8be4f4cf3c8713989c70b883a92c1f4
Den <denis100703@gmail.com>
2022-03-28 17:32:16 +0300

    lab 8 dop ready

--- Semester_2/Lab_8/dop/main.cpp
+++ Semester_2/Lab_8/dop/main.cpp
@@ -6,14 +6,42 @@
 // доп: реализовать алгоритм быстрой сортировки
 // и сравнить скорость их выполнения
 
+#include <random>
+#include <chrono>
 #include "sort.h"
-#define N 15 // кол-во элементов
+
+using namespace std;
+#define N 50000 // кол-во элементов
+#define OUTPUT 0 // нужно ли принтовать массив
 
 int main() {
-  int arr[N] = {7,2,12,16,8,17,11,13,4,1,15,20,19,3,18};
-  printf("%-11s","original:"); print_arr(arr, N);
+  int arr[N] = {0}, temp[N] = {0};
+  for (int i = 0; i < N; i++)
+    arr[i] = rand() % (N * 10);
 
-  // сортируем массив
-  qsort(arr, N);
-  printf("%-11s","sorted:"); print_arr(arr, N);
+  #if OUTPUT == 1
+    printf("%-11s", "original:");
+    print_arr(arr, N);
+  #endif
+
+  // измряем время сортировки вставками
+  for (int i = 0; i < N; i++) temp[i] = arr[i];
+  auto begin = chrono::steady_clock::now();
+  sort(arr, N); // сортировка вставками
+  auto end = chrono::steady_clock::now();
+  auto time = chrono::duration_cast<chrono::milliseconds>(end - begin);
+  printf("Sort time elapsed: %lld ms\n", time.count());
+
+  // измряем время быстрой сортировки
+  for (int i = 0; i < N; i++) temp[i] = arr[i];
+  begin = chrono::steady_clock::now();
+  qsort(arr, N); // быстрая сортировка
+  end = chrono::steady_clock::now();
+  time = chrono::duration_cast<chrono::milliseconds>(end - begin);
+  printf("qSort time elapsed: %lld ms\n", time.count());
+
+  #if OUTPUT == 1
+    printf("%-11s", "sorted:");
+    print_arr(arr, N);
+  #endif
 }
