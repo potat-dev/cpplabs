c94e8fe2a95ec0d31b55bc54f2ad97062b24e014
Den <denis100703@gmail.com>
2022-04-04 09:57:32 +0300

    fine comments in Lab 7 var 12

--- Variants/Lab_7/var_12/frand.h
+++ Variants/Lab_7/var_12/frand.h
@@ -1,6 +1,11 @@
 #pragma once
 
-void rand_init();
+// выдает случайное число из промежутка [0..1]
 #define rand_1 ((float)(rand()) / (RAND_MAX))
 
+// инициализация генератора
+void rand_init();
+
+// генератор нормально распределенного
+// случайного числа в промежутке [0..n]
 double frand(double n, int count);
