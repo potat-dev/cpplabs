23ccbfbe9a4685d53778dc3c9245aaa7f72f3d02
Den <denis100703@gmail.com>
2022-04-08 23:17:59 +0300

    FileHeader reading added

--- Practice/Lab_15/main.cpp
+++ Practice/Lab_15/main.cpp
@@ -1,113 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib> // для использования функции exit()
+#include <cstdlib> // для функции exit()
 
-/*
-  Заголовок файла (BitMapFileHeader)
-  Type	2	0	Сигнатура "BM"
-  Size	4	2	Размер файла
-  Reserved 1	2	6	Зарезервировано
-  Reserved 2	2	8	Зарезервировано
-  OffsetBits	4	10	Смещение изображения от начала файла
+using namespace std;
 
-  Информационный заголовок (BitMapInfoHeader)
-  Size	4	14	Длина заголовка
-  Width	4	18	Ширина изображения, точки
-  Height	4	22	Высота изображения, точки
-  Planes	2	26	Число плоскостей
-  BitCount	2	28	Глубина цвета, бит на точку
-  Compression	4	30	Тип компрессии (0 - несжатое изображение)
-  SizeImage	4	34	Размер изображения, байт
-  XpelsPerMeter	4	38	Горизонтальное разрешение, точки на метр
-  YpelsPerMeter	4	42	Вертикальное разрешение, точки на метр
-  ColorsUsed	4	46	Число используемых цветов (0 - максимально возможное для данной глубины цвета)
-  ColorsImportant	4	50	Число основных цветов
-*/
+struct BitMapFileHeader {
+  uint16_t Type;             //  2  0   Сигнатура "BM"
+  uint32_t Size;             //  4  2   Размер файла
+  uint16_t Reserved1;        //  2  6   Зарезервировано
+  uint16_t Reserved2;        //  2  8   Зарезервировано
+  uint32_t OffsetBits;       //  4  10  Смещение изображения от начала файла
+};
 
-typedef struct {
-  uint16_t  Type;             //  2  0   Сигнатура "BM"
-  uint32_t  Size;             //  4  2   Размер файла
-  uint8_t   Reserved;         //  2  6   Зарезервировано
-  uint16_t  Reserved;         //  2  8   Зарезервировано
-  uint32_t  OffsetBits;       //  4  10  Смещение изображения от начала файла
-} BitMapFileHeader;
+struct BitMapInfoHeader {
+  uint32_t Size;             //  4  14  Длина заголовка
+  uint32_t Width;            //  4  18  Ширина изображения, точки
+  uint32_t Height;           //  4  22  Высота изображения, точки
+  uint16_t Planes;           //  2  26  Число плоскостей
+  uint16_t BitCount;         //  2  28  Глубина цвета, бит на точку
+  uint32_t Compression;      //  4  30  Тип компрессии
+  uint32_t SizeImage;        //  4  34  Размер изображения, байт
+  uint32_t XpelsPerMeter;    //  4  38  Горизонтальное разрешение, точки на метр
+  uint32_t YpelsPerMeter;    //  4  42  Вертикальное разрешение, точки на метр
+  uint32_t ColorsUsed;       //  4  46  Число используемых цветов
+  uint32_t ColorsImportant;  //  4  50  Число основных цветов
+};
 
-typedef struct {
-  uint32_t  Size;             //  4  14  Длина заголовка
-  uint32_t  Width;            //  4  18  Ширина изображения, точки
-  uint32_t  Height;           //  4  22  Высота изображения, точки
-  uint16_t  Planes;           //  2  26  Число плоскостей
-  uint16_t  BitCount;         //  2  28  Глубина цвета, бит на точку
-  uint32_t  Compression;      //  4  30  Тип компрессии
-  uint32_t  SizeImage;        //  4  34  Размер изображения, байт
-  uint32_t  XpelsPerMeter;    //  4  38  Горизонтальное разрешение, точки на метр
-  uint32_t  YpelsPerMeter;    //  4  42  Вертикальное разрешение, точки на метр
-  uint32_t  ColorsUsed;       //  4  46  Число используемых цветов
-  uint32_t  ColorsImportant;  //  4  50  Число основных цветов
-} BitMapInfoHeader;
+uint32_t get(FILE *f, uint8_t bytes) {
+  uint32_t temp = 0;
+  for (int i = 0; i < bytes; i++)
+    temp |= getc(f) << (8 * i); // little-endian
+  return temp;
+}
 
-typedef struct {
-
-} ColorTable;
-
-typedef struct {
-
-} BitMap;
 
 int main() {
-	using namespace std;
- 
-	// ifstream используется для чтения содержимого файла.
-	// Попытаемся прочитать содержимое файла SomeText.txt
-	ifstream inf("input.bmp", ios::binary);
- 
-	// Если мы не можем открыть этот файл для чтения его содержимого,
-	if (!inf) {
-		// то выводим следующее сообщение об ошибке и выполняем функцию exit()
-    cerr << "could not be opened for reading" << endl;
-		exit(1);
-	}
+  FILE * in_file = fopen("input.bmp", "rb");
 
-	// Пока есть данные, которые мы можем прочитать,
-	while (inf) {
-		// то перемещаем эти данные в строку, которую затем выводим на экран
-    string strInput;
-		getline(inf, strInput);
-		cout << strInput << endl;
-	}
-}
-
-
-static unsigned short read_u16(FILE *fp) {
-    unsigned char b0, b1;
-    b0 = getc(fp);
-    b1 = getc(fp);
-    return ((b1 << 8) | b0);
-}
- 
- 
-static unsigned int read_u32(FILE *fp)
-{
-    unsigned char b0, b1, b2, b3;
- 
-    b0 = getc(fp);
-    b1 = getc(fp);
-    b2 = getc(fp);
-    b3 = getc(fp);
- 
-    return ((((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
-}
- 
- 
-static int read_s32(FILE *fp)
-{
-    unsigned char b0, b1, b2, b3;
- 
-    b0 = getc(fp);
-    b1 = getc(fp);
-    b2 = getc(fp);
-    b3 = getc(fp);
- 
-    return ((int)(((((b3 << 8) | b2) << 8) | b1) << 8) | b0);
+  BitMapFileHeader file_header;
+  file_header.Type       = get(in_file, sizeof(file_header.Type));
+  file_header.Size       = get(in_file, sizeof(file_header.Size));
+  file_header.Reserved1  = get(in_file, sizeof(file_header.Reserved1));
+  file_header.Reserved2  = get(in_file, sizeof(file_header.Reserved2));
+  file_header.OffsetBits = get(in_file, sizeof(file_header.OffsetBits));
+  cout << "Type:       " << hex << file_header.Type       << endl;
+  cout << "Size:       " << dec << file_header.Size       << endl;
+  cout << "Reserved1:  " << hex << file_header.Reserved1  << endl;
+  cout << "Reserved2:  " << hex << file_header.Reserved2  << endl;
+  cout << "OffsetBits: " << dec << file_header.OffsetBits << endl;
 }
