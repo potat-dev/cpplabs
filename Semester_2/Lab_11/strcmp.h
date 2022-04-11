66757bd9d459308442b2d6486b2a1f1510fc8f14
Den <denis100703@gmail.com>
2022-04-11 20:57:40 +0300

    formatted Lab 11 & dop

--- Semester_2/Lab_11/strcmp.h
+++ Semester_2/Lab_11/strcmp.h
@@ -1,6 +1,10 @@
 int strcmp(const char *s1, const char *s2) {
   while (*s1 && (*s1 == *s2)) {
-    s1++; s2++;
+    s1++;
+    s2++;
   }
-  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+  return *(const char *)s1 - *(const char *)s2;
 }
+
+// от последнего символа 2 строки отнимается
+// последний символ 1 строки
--- Semester_2/Lab_11/strpbrk.h
+++ Semester_2/Lab_11/strpbrk.h
@@ -1,10 +1,15 @@
-const char *strpbrk(const char *string, const char *strCharSet) {
+#define NULL __null
+
+const char *strpbrk(const char *string, const char *chars) {
   char *temp = (char *)string;
   while (*temp != 0) {
-    for (int i = 0; strCharSet[i] != 0; i++)
-      if (*temp == strCharSet[i])
+    for (int i = 0; chars[i] != 0; i++)
+      if (*temp == chars[i])
         return temp;
     temp++;
   }
   return NULL;
 }
+
+// ищет в строке string символы из строки chars
+// возвращает указатель на первый найденный символ в строке string
