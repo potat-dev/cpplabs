11055455a23177a11279c0c9edbf9e6e5e3c70dc
Den <denis100703@gmail.com>
2022-04-11 21:07:03 +0300

    Lab 7 var 11 fixed - normal random

--- Test/other_variants/Lab_7/var_11/frand.cpp
+++ Test/other_variants/Lab_7/var_11/frand.cpp
@@ -1,20 +1,33 @@
 #include "frand.h"
 
+// центральная предельная теорема
 float normal_frand_n(int count, int n) {
   float sum = 0;
+  // находим среднее арифметическое
   for (int i = 0; i < count; i++)
     sum += frand_n(n);
   return sum / count;
 }
 
+// индекс нужного интервала
+int round(float f, int N) {
+  if (f > 0) {
+    return (int) f + N;
+  } else {
+    return N + (int) f - 1;
+  }
+}
+
+// красивый вывод на экран
 void pretty_print(int *arr, int N, int wide) {
   int max = 0;
+  // вычисление коэффициента максимальной 
   for (int i = 0; i < N * 2; i++)
     if (arr[i] > max) max = arr[i];
   int w = max / wide;
-  
+  // выводим график на экран
   for (int i = 0; i < N * 2; i++) {
-    printf("(%2d, %2d)  %6d | ", i-N, i-N+1, arr[i]);
+    printf("[%2d, %2d]  %6d | ", i-N, i-N+1, arr[i]);
     for (int j = 0; j < arr[i] / w; j++) printf("#");
     printf("\n");
   } 
