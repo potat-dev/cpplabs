7ce187f48c3a8bddbc59e2206006d5af01cd109b
Den <denis100703@gmail.com>
2022-04-14 21:22:38 +0300

    Lab 9 var 11 ready

--- Variants/Lab_9/var_11/sequence.cpp
+++ Variants/Lab_9/var_11/sequence.cpp
@@ -6,9 +6,11 @@ int count_bits(unsigned long long N) {
   // определяем минимальную но достаточную..
   // степень двойки (= кол-во бит)
   while (N >= i) {
-    p += 1; // увеличиваем количество бит
-    i *= 2; // максимально возможное число при..
-    // данном количестве бит теперь в 2 раза больше
+    // увеличиваем количество бит
+    p += 1; 
+    // максимально возможное число при данном
+    // количестве бит теперь в 2 раза больше
+    i *= 2;
   }
   return p;
 }
@@ -41,19 +43,17 @@ int count_max_ones(unsigned long long N) {
   int max = 0, current = 0;
 
   // проходимся по всем битам числа
-  for (int i = 0; bin[i]; i++) {
-    current = bin[i] == '1' ? current + 1 : 0;
-    // if (bin[i] == '1') {
-    //   current += 1;
-    // } else {
-    //   current = 0;      
-    // }
-
-    // если текущая последовательность длиннее максимальной,
-    // то максимальную приравниваем к текущей
-    max = current > max ? current : max;
-    // if (current > max)
-    //   max = current;
+  for (int i = 0; bin[i] != 0; i++) {
+    // current = bin[i] == '1' ? current + 1 : 0;
+    if (bin[i] == '1') {
+      current += 1;
+    } else {
+      current = 0;      
+    }
+    // обновляем максимальную длину
+    // max = current > max ? current : max;
+    if (current > max)
+      max = current;
   }
   return max;
 }
