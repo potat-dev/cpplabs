6437b88f9d4504524b57682eb341762288d7e94b
Den <denis100703@gmail.com>
2022-04-22 22:10:46 +0300

    Lab 9 var 12 fine comments added

--- Variants/Lab_9/var_12/lab.h
+++ Variants/Lab_9/var_12/lab.h
@@ -3,8 +3,14 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+// инвертирует каждый четвертый бит числа
 unsigned int invert_4(unsigned int N);
+
+// инвертирует каждый N-ый бит числа (32 bit)
 unsigned int invert_every_nth(unsigned int N, unsigned int bit);
+
+// определяет, каждый какой бит инвертирован
 unsigned int get_invert_offset(unsigned int a, unsigned int b);
 
+// красивый вывод двоичного числа на экран
 void print_bin(unsigned int N);
--- Variants/Lab_9/var_12/main.cpp
+++ Variants/Lab_9/var_12/main.cpp
@@ -2,13 +2,17 @@
 // полученное из введенного инверсией каждого
 // 4-го двоичного разряда (начиная с младших)
 
+// доп: определить, каждый какой бит инвертирован
+
 #include "lab.h"
 
 int main() {
+  // вводим сдвиг для инвертирования
   unsigned int offset;
   printf("offset: ");
   scanf("%u", &offset);
 
+  // вводим исходное число
   unsigned int in;
   printf("input: ");
   scanf("%u", &in);
@@ -16,11 +20,13 @@ int main() {
   print_bin(in);
   printf("\n\n");
 
+  // инвертируем биты числа
   unsigned int out = invert_every_nth(in, offset);
   printf("output: %u\nbin: ", out);
   print_bin(out);
   printf("\n\n");
 
+  // определяем разницу и сдвиг
   printf("dif: ");
   print_bin(in ^ out);
   printf("\noffset: %d\n", get_invert_offset(in, out));
