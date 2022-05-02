aaea50b4376f7b368f73db287337fc178ae1a8d6
Den <denis100703@gmail.com>
2022-05-02 21:04:25 +0300

    added a check for a non-square matrix

--- Variants/Lab_12/var_12/main.cpp
+++ Variants/Lab_12/var_12/main.cpp
@@ -12,21 +12,43 @@
 
 #include <vector>
 #include <iostream>
+#include <cstdlib> // для exit()
+
 using namespace std;
+
+unsigned int get_size(vector <vector <double>> arr) {
+  unsigned int size = arr.size();
+  for (int i = 0; i < size; i++) {
+    if (arr[i].size() != size) {
+      return 0;
+    }
+  }
+  return size;
+}
 
 class Matrix {
   private:
     vector <vector <double>> arr;
 
   public:
-    Matrix(int size = 1) {
-      arr.resize(size);
-      for (int i = 0; i < size; i++)
-        arr[i].resize(size);
+    Matrix(unsigned int size = 1) {
+      try {
+        if (size > 0) {
+          arr.resize(size);
+          for (int i = 0; i < size; i++) {
+            arr[i].resize(size);
+          }
+        } else {
+          throw "Matrix is not square!";
+        }
+      } catch (const char *s) {
+        cerr << "Error in class constructor: " << s << endl;
+        exit(1);
+      }
     }
     
-    Matrix(vector <vector <double>> a) {
-      arr = a;
+    Matrix(vector <vector <double>> temp) : Matrix (get_size(temp)) {
+      arr = temp;
     }
 
     Matrix& set(int x, int y, double value) {}
@@ -61,13 +83,26 @@ int main() {
   );
   m3.print();
 
-  //TODO: делать проверку на квадратность
-  Matrix m4(
+  Matrix m4(m3);
+  m4.print();
+
+  Matrix m5(
+    {
+      {1, 2},
+      {3, 4}
+    }
+  );
+  m5.print();
+
+  m5 = m3;
+  m5.print();
+
+  Matrix m6(
     {
       {1, 1, 0, 0, 1},
       {1, 1, 0, 0, 1},
       {1, 1, 0, 0, 1},
     }
   );
-  m4.print();
+  m6.print();
 }
