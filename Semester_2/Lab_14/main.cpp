869e7bb28863d2eb540ab805f03441519cf66704
Den <denis100703@gmail.com>
2022-05-04 13:27:45 +0300

    Lab 14 template added

--- Semester_2/Lab_14/main.cpp
+++ Semester_2/Lab_14/main.cpp
@@ -11,3 +11,62 @@
 // Класс FigureList должен быть основан на связном списке.
 // Связаный список должен быть реализован с помощью двух классов
 // Node (элемент списка) и List (сам список)
+
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Shape {
+  protected:
+    int id;
+    int x, y;
+
+  public:
+    Shape(int id, int x, int y);
+    void print();
+};
+
+class Circle : public Shape {
+  protected:
+    int r;
+    string text;
+
+  public:
+    Circle(int id, int x, int y, int r, string text);
+    void print();
+};
+
+class Segment : public Shape {
+  protected:
+    int x_start, y_start, x_end, y_end;
+  
+  public:
+    Segment(int id, int x_start, int y_start, int x_end, int y_end);
+    void print();
+};
+
+class FigureList {
+  private:
+    Shape* arr[100];
+    int size = 0;
+
+  public:
+    void add(Shape* s);
+
+    Shape &get(int id);
+
+    // найти фигуру всписке по идентификатору
+    Shape& findFigure(int id);
+
+    // удалить фигуру из списка
+    void erase(int id);
+
+    // вывести на экран в текстовом режиме информацию о всех фигурах в списке
+    void printAll();
+};
+
+
+int main() {
+  FigureList list;
+}
