1d4dbdddd3ef0a0b54dca31f58938476ba92ac5c
Den <denis100703@gmail.com>
2022-05-17 01:34:16 +0300

    original files of Lab 12 var 6 ver 2

---
+++ HugeInt.h
@@ -0,0 +1,29 @@
+#ifndef HUGEINT_H
+#define HUGEINT_H
+
+#include <string>
+#include <iostream>
+
+class HugeInt {
+private:
+	int number[40];
+	int depth;
+	bool negative;
+public:
+	HugeInt();
+	HugeInt(int depth);
+	HugeInt(int numbers[40], int depth);
+
+	void set(std::string str);
+	std::string get();
+
+	bool check_zero();
+	int compare(HugeInt& b);
+	HugeInt simple_sum(HugeInt& b);
+	HugeInt simple_dif(HugeInt& b);
+	HugeInt sum(HugeInt& b);
+	HugeInt dif(HugeInt& b);
+
+};
+
+#endif
---
+++ L12_6.cpp
@@ -0,0 +1,51 @@
+#include "HugeInt.h"
+
+/*Создайте класс HugeInt, который хранит в 40-элементном массиве цифр целые числа разрядностью до 40 знаков.
+Разрядность указать в конструкторе. Предусмотреть функции сложения, вычитания, сравнения и проверки на 0.
+*/
+/*
+1)НЕ ДОП:
+расставить const
+сет и гет для всех полей
+узнавать знак числа
+менять знак числа
+узнавать позицию на разряде
+менять позицию на разряде
+
+урезать размер числа, чтобы не было 0
+2)ДОП:
+перегрузка операторов
+умножение
+нахождение остатка от деления
+*/
+
+using namespace std;
+int main() {
+
+	HugeInt a;
+	HugeInt b;
+	HugeInt s;
+
+	a.set("1000");
+	b.set("7");
+	
+	if (a.check_zero() == true) { cout << a.get() << " - its zero " << endl; }
+	else { cout << a.get() << " - not zero " << endl; }
+	if (b.check_zero() == true) { cout << b.get() << " - its zero " << endl; }
+	else { cout << b.get() << " - not zero " << endl; }
+	
+	if (a.compare(b) == 1) { cout << a.get() << " > " << b.get() << endl; }
+	else if (a.compare(b) == -1 ) { cout << a.get() << " < " << b.get() << endl; }
+	else if (a.compare(b) == 0) { cout << a.get() << " = " << b.get() << endl; }
+
+	s = a.dif(b);
+	cout << "Difference = ";
+	cout << s.get() << endl;
+	s = a.sum(b);
+	cout << "SUM = ";
+	cout << s.get() << endl;
+
+
+
+	return 0;
+}
