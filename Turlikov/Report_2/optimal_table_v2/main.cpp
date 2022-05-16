685e2b8041c8e7f6ddf5f06a7b5dabdf699c33a3
Den <denis100703@gmail.com>
2022-05-16 19:00:26 +0300

    hamming table generator improved to longer words

--- Turlikov/Report_2/optimal_table_v2/main.cpp
+++ Turlikov/Report_2/optimal_table_v2/main.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <math.h>
-#define N 5
-#define D 3
+#include <memory.h>
+#define N 25
+#define D 20
 
 #define FREE 0
 #define BAD  1
 #define USED 2
 
-unsigned char count_ones(unsigned char n) {
+unsigned char count_ones(unsigned long long n) {
   unsigned char w = 0;
   while (n > 0) {
     w += n & 1;
@@ -16,32 +16,35 @@ unsigned char count_ones(unsigned char n) {
   return w;
 }
 
-bool has_free(int *arr, int size) {
-  for (int i = 0; i < size; i++)
+bool has_free(unsigned long long *arr, unsigned long long size) {
+  for (unsigned long long i = 0; i < size; i++)
     if (arr[i] == FREE) return true;
   return false;
 }
 
 int main() {
-  int count = 0;               // счетчик слов в кодовой книге
-  int next_free = 0;           // следующее свободное слово
-  int size = pow(2, N);        // максимальное количество кодовых слов
-  int mark[size] = {FREE};     // отметки об использовании кодовых слов
-  int CodeBook[size] = {FREE}; // кодовая книга
+  unsigned long long count = 0;               // счетчик слов в кодовой книге
+  unsigned long long next_free = 0;           // следующее свободное слово
+  unsigned long long size = (unsigned long long)1 << N;        // максимальное количество кодовых слов
+  unsigned long long *mark = new unsigned long long[size];     // отметки об использовании кодовых слов
+  unsigned long long *CodeBook = new unsigned long long[size]; // кодовая книга
+  for (unsigned long long i = 0; i < size; i++) {
+    mark[i] = CodeBook[i] = 0;
+  }
 
   CodeBook[count] = 0;            // выбираем первое кодовое слово
   mark[CodeBook[count++]] = USED; // отметим его в массиве пометок
 
   while (has_free(mark, size)) {
     // проходимся по всем словам
-    for (int i = 0; i < size; i++) {
+    for (unsigned long long i = 0; i < size; i++) {
       // если слово свободно и его расстояние Хэмминга меньше чем нужно
       if (mark[i] == FREE && (count_ones(i ^ CodeBook[count - 1]) < D)) {
         mark[i] = BAD; // то отмечаем как неподходящее
       }
-      printf("%d ", mark[i]); // выводим на экран
+      // printf("%d ", mark[i]); // выводим на экран
     }
-    printf("\n");
+    // printf("\n");
 
     // выбираем следующее кодовое слово
     for (next_free = 0; mark[next_free] != FREE; next_free++);
@@ -51,7 +54,7 @@ int main() {
 
   // выводим кодовую книгу на экран
   printf("\ncode: ");
-  for (int i = 0; i < count; i++) {
+  for (unsigned long long i = 0; i < count; i++) {
     printf("%d ", CodeBook[i]);
   }
   
