caac2565d41f83bfab99571d3bd809fd4255b320
Den <denis100703@gmail.com>
2022-05-18 14:13:35 +0300

    all Hamming functions rewrited to uint64

---
+++ Turlikov/Report_2/hamming.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <stdio.h>
+
+typedef unsigned long long uint64;
+typedef unsigned char uint8;
+
+static uint64 code_table[] = {0x00, 0x0d, 0x16, 0x1b}; //1f
+
+uint64 coder(uint64 m) {
+  return code_table[m];
+}
+
+uint64 channel(uint64 a, uint64 e) {
+  return a ^ e;
+}
+
+uint8 count_ones(uint64 n) {
+  uint8 w = 0;
+  while (n > 0) {
+    w += n & 1;
+    n = n >> 1;
+  }
+  return w;
+}
+
+uint64 hamming(uint64 a, uint64 b) {
+  return count_ones(a ^ b);
+}
+
+uint64 decoder(uint64 m) {
+  uint64 ind = 0, min = 5;
+  for (uint64 i = 0; i < 4; i++) {
+    uint64 d = hamming(m, code_table[i]);
+    if (d < min) {
+      min = d;
+      ind = i;
+    }
+  }
+  return ind;
+}
+
+void printf_bin(uint64 n) {  
+  for (uint64 i = 0; i < 5; i++)
+    printf("%d", (n >> (4 - i)) & 1);
+}
--- Turlikov/Report_2/min_hamming/main.cpp
+++ Turlikov/Report_2/min_hamming/main.cpp
@@ -1,5 +1,18 @@
-#include "hamming.h"
-#include "stdio.h"
+#include "..\hamming.h"
+// #define DOP
+
+int min_hamming(uint64* arr, int size) {
+  int minim = size;
+  for (uint64 a = 0; a < size; a++) {
+    for (uint64 b = a + 1; b < size; b++) {
+      if (arr[a] != arr[b]) {
+        int hamm = count_ones(arr[a] ^ arr[b]);
+        minim = hamm < minim ? hamm : minim;
+      }
+    }
+  }
+  return minim;
+}
 
 int main() {
   printf(
@@ -7,19 +20,20 @@ int main() {
     min_hamming(code_table, 4)
   );
 
-  int max = 32;
-  int count = 0;
-  for (unsigned char a = 0; a < max; a++) {
-    for (unsigned char b = a; b < max; b++) {
-      if (a != b) {
-        printf("\na: "); printf_bin(a);
-        printf(" b: "); printf_bin(b);
-        printf(" hamming: "); printf_bin(hamming(a, b));
-        printf(" dif: %d", hamming(a, b));
-        count += 1;
+  #ifdef DOP
+    int max = 32;
+    int count = 0;
+    for (uint64 a = 0; a < max; a++) {
+      for (uint64 b = a; b < max; b++) {
+        if (a != b) {
+          printf("\na: "); printf_bin(a);
+          printf(" b: "); printf_bin(b);
+          printf(" hamming: "); printf_bin(hamming(a, b));
+          printf(" dif: %d", hamming(a, b));
+          count += 1;
+        }
       }
     }
-  }
-  
-  printf("\ncount: %d", count);
+    printf("\ncount: %d", count);
+  #endif
 }
