ff3c7f2724b8a607dd629b1cdec29e0b52dbee20
Den <denis100703@gmail.com>
2022-05-18 14:38:14 +0300

    minor code formatting

--- Turlikov/Report_2/hamming.h
+++ Turlikov/Report_2/hamming.h
@@ -4,18 +4,10 @@
 typedef unsigned long long uint64;
 typedef unsigned char uint8;
 
-static uint64 code_table[] = {0x00, 0x0d, 0x16, 0x1b}; //1f
+static uint64 code_table[] = {0x00, 0x0d, 0x16, 0x1b /* 0x1f */};
 
-uint64 coder(uint64 m) {
-  return code_table[m];
-}
-
-uint64 channel(uint64 a, uint64 e) {
-  return a ^ e;
-}
-
-uint8 count_ones(uint64 n) {
-  uint8 w = 0;
+uint64 count_ones(uint64 n) {
+  uint64 w = 0;
   while (n > 0) {
     w += n & 1;
     n = n >> 1;
@@ -23,9 +15,9 @@ uint8 count_ones(uint64 n) {
   return w;
 }
 
-uint64 hamming(uint64 a, uint64 b) {
-  return count_ones(a ^ b);
-}
+uint64 coder(uint64 m) { return code_table[m]; }
+uint64 channel(uint64 a, uint64 e) { return a ^ e; }
+uint64 hamming(uint64 a, uint64 b) { return count_ones(a ^ b); }
 
 uint64 decoder(uint64 m) {
   uint64 ind = 0, min = 5;
--- Turlikov/Report_2/optimal_table_v2/main.cpp
+++ Turlikov/Report_2/optimal_table_v2/main.cpp
@@ -1,62 +1,44 @@
-#include <stdio.h>
-#include <memory.h>
-#define N 25
-#define D 20
+#include "..\hamming.h"
+#include <iostream>
+using namespace std;
+
+#define N 16 // количесво бит в кодовом слове
+#define D 8  // минимальное расстояние хэмминга для кодовой книги
 
 #define FREE 0
 #define BAD  1
 #define USED 2
 
-unsigned char count_ones(unsigned long long n) {
-  unsigned char w = 0;
-  while (n > 0) {
-    w += n & 1;
-    n = n >> 1;
-  }
-  return w;
-}
-
-bool has_free(unsigned long long *arr, unsigned long long size) {
-  for (unsigned long long i = 0; i < size; i++)
+bool has_free(uint64 *arr, uint64 size) {
+  for (uint64 i = 0; i < size; i++)
     if (arr[i] == FREE) return true;
   return false;
 }
 
 int main() {
-  unsigned long long count = 0;               // счетчик слов в кодовой книге
-  unsigned long long next_free = 0;           // следующее свободное слово
-  unsigned long long size = (unsigned long long)1 << N;        // максимальное количество кодовых слов
-  unsigned long long *mark = new unsigned long long[size];     // отметки об использовании кодовых слов
-  unsigned long long *CodeBook = new unsigned long long[size]; // кодовая книга
-  for (unsigned long long i = 0; i < size; i++) {
-    mark[i] = CodeBook[i] = 0;
-  }
+  uint64 count = 0;     // счетчик слов в кодовой книге
+  uint64 next_free = 0; // следующее свободное слово
+  uint64 size = (uint64) 1 << N;        // максимальное количество кодовых слов
+  uint64 *mark = new uint64[size];     // отметки об использовании кодовых слов
+  uint64 *CodeBook = new uint64[size]; // кодовая книга
+  for (uint64 i = 0; i < size; mark[i++] = CodeBook[i] = 0);
 
   CodeBook[count] = 0;            // выбираем первое кодовое слово
   mark[CodeBook[count++]] = USED; // отметим его в массиве пометок
 
   while (has_free(mark, size)) {
-    // проходимся по всем словам
-    for (unsigned long long i = 0; i < size; i++) {
-      // если слово свободно и его расстояние Хэмминга меньше чем нужно
-      if (mark[i] == FREE && (count_ones(i ^ CodeBook[count - 1]) < D)) {
+    for (uint64 i = 0; i < size; i++) // проходимся по всем словам
+      if (mark[i] == FREE && (hamming(i, CodeBook[count - 1]) < D)) // если слово свободно и его расстояние Хэмминга меньше чем нужно
         mark[i] = BAD; // то отмечаем как неподходящее
-      }
-      // printf("%d ", mark[i]); // выводим на экран
-    }
-    // printf("\n");
-
     // выбираем следующее кодовое слово
     for (next_free = 0; mark[next_free] != FREE; next_free++);
     CodeBook[count] = next_free;    // заносим это слово в словарь
     mark[CodeBook[count++]] = USED; // отметим это слово в массиве отметок
   }
 
-  // выводим кодовую книгу на экран
-  printf("\ncode: ");
-  for (unsigned long long i = 0; i < count; i++) {
-    printf("%d ", CodeBook[i]);
-  }
-  
+  printf("\ncode: "); // выводим кодовую книгу на экран
+  for (uint64 i = 0; i < count; i++)
+    cout << CodeBook[i] << " ";
+
   return 0;
 }
