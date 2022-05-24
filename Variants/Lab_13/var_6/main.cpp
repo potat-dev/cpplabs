134e044b116ab5e664aeab2d2799bac934304cdc
Den <denis100703@gmail.com>
2022-05-25 02:53:12 +0300

    code reformatted & Lab ready

--- Variants/Lab_13/var_6/main.cpp
+++ Variants/Lab_13/var_6/main.cpp
@@ -7,203 +7,197 @@
 // перегруженные операторы ввода и вывода
 // конструктор копий
 // оператор присваивания
-
 // указанные в задании перегруженные операции
 // ++ -- -n == != = + += - -= * *= / /= > < >= <=
 
-//  НЕ ДОП:
-//*  - поправить вывод
-//*  - !!!учесть отрицательные числа
-//  ДОП:
+// Исправить:
+//* - поправить вывод
+//* - !!!учесть отрицательные числа
+
+// Доп:
 //* - добавить целую часть (в сам класс)
 //* - операции, соотв., с учетом целой части дробей
-//*  - сортировка Шелла массива дробей
+//* - сортировка Шелла массива дробей
 
 #include "rational.h"
 using namespace std;
 
 // тестирование функций класса
-int main()
-{
+int main() {
   int test_n = 0;
   cout << "Run test #";
   cin >> test_n;
 
-  switch (test_n)
-  {
-  case 0: // test
-  {
-    Rational r1(-11205729, 14420), r2(3349, 14420), r3(11207689, 14420), r4(5439, 103);
-    cout << r1 << " = " << r1.normalize() << " = " << r1.normalize() << " = " << r1.denormalize() << endl;
-    cout << r2 << " = " << r2.normalize() << " = " << r2.normalize() << " = " << r2.denormalize() << endl;
-    cout << r3 << " = " << r3.normalize() << " = " << r3.normalize() << " = " << r3.denormalize() << endl;
-    cout << r4 << " = " << r4.normalize() << " = " << r4.normalize() << " = " << r4.denormalize() << endl;
-    break;
-  }
-  case 1:
-  {
-    // тут тестируются:
-    // конструкторы, геттеры, сеттеры,
-    // вывод, конструктор копий, присваивания
-
-    Rational lol(1, 2), kek = {2, 3};
-    cout << "Rational 1 / 2: " << lol << endl;
-    cout << "Rational 2 / 3: " << kek << endl;
-
-    Rational temp(618);
-    cout << "Int 618 to Rational: " << temp << endl;
-
-    Rational test(42, 618);
-    cout << "42 / 618 = " << test << " -> ";
-
-    test.set(618, 42);
-    cout << test << " -> ";
-    cout << test.numerator() << " / "
-         << test.denominator() << " -> ";
-
-    test.numerator() = 6;
-    test.denominator() = 18;
-    cout << test << endl;
-
-    Rational test_copy(test);
-    cout << test << " = " << test.simplify()
-         << " => copy: " << test_copy << endl;
-
-    lol = kek = temp = test;
-    cout << lol << ", "
-         << kek << ", "
-         << temp << endl;
-    break;
-  }
-
-  case 2:
-  {
-    // тут тестируются:
-    // ++ -- -n + += - -= * *= / /=
-
-    Rational lol(42, 618);
-    Rational kek(69, 420);
-    long long abc = 777;
-
-    cout << "++" << lol << " = " << ++lol << "\n";
-    cout << "--" << lol << " = " << --lol << "\n";
-    cout << "-" << lol << " = " << -lol << "\n";
-
-    cout << lol << " + " << kek << " = " << lol + kek << endl;
-    cout << lol << " + " << abc << " = " << lol + abc << endl;
-    cout << abc << " + " << lol << " = " << abc + lol << endl;
-
-    cout << lol << " - " << kek << " = " << lol - kek << endl;
-    cout << lol << " - " << abc << " = " << lol - abc << endl;
-    cout << abc << " - " << lol << " = " << abc - lol << endl;
-
-    cout << lol << " * " << kek << " = " << (lol * kek).normalize() << endl;
-    cout << lol << " * " << abc << " = " << (lol * abc).normalize() << endl;
-    cout << abc << " * " << lol << " = " << (abc * lol).normalize() << endl;
-
-    cout << lol << " / " << kek << " = " << lol / kek << endl;
-    cout << lol << " / " << abc << " = " << lol / abc << endl;
-    cout << abc << " / " << lol << " = " << abc / lol << endl;
-
-    lol = {42, 618};
-    kek = {69, 420};
-    abc = 777;
-    cout << lol << " += " << kek << " -> " << (lol += kek) << endl;
-    cout << lol << " += " << abc << " -> " << (lol += abc) << endl;
-
-    lol = {42, 618};
-    kek = {69, 420};
-    abc = 777;
-    cout << lol << " -= " << kek << " -> " << (lol -= kek) << endl;
-    cout << lol << " -= " << abc << " -> " << (lol -= abc) << endl;
-
-    lol = {42, 618};
-    kek = {69, 420};
-    abc = 777;
-    cout << lol << " *= " << kek << " -> " << (lol *= kek) << endl;
-    cout << lol << " *= " << abc << " -> " << (lol *= abc) << endl;
-
-    lol = {42, 618};
-    kek = {69, 420};
-    abc = 777;
-    cout << lol << " /= " << kek << " -> " << (lol /= kek) << endl;
-    cout << lol << " /= " << abc << " -> " << (lol /= abc) << endl;
-
-    break;
-  }
-
-  case 3:
-  {
-    // тут тестируются:
-    // == != > >= < <=
-
-    Rational testA(1, 2);
-    Rational testB(1, 2);
-
-    if (testA == testB)
-      cout << "testA == testB" << endl;
-    if (testA == testA)
-      cout << "testA == testA" << endl;
-
-    Rational testNew(testA);
-    if (testA == testNew)
-      cout << "testA == testNew" << endl;
-
-    cout << ++testNew << ", " << testNew << endl;
-    if (testA == testNew)
-      cout << "testA == testNew" << endl;
-
-    cout << testA++ << ", " << testA << endl;
-    if (testA == testNew)
-      cout << "testA == testNew" << endl;
-
-    Rational temp(10, -20);
-    cout << temp << ", " << temp.simplify() << ", " << temp << endl;
-
-    Rational lol = {42, 618}, kek = {69, 420};
-    cout << lol << " > " << kek << (lol > kek ? " = True" : " = False") << endl;
-    cout << lol << " >= " << kek << (lol >= kek ? " = True" : " = False") << endl;
-    cout << lol << " < " << kek << (lol < kek ? " = True" : " = False") << endl;
-    cout << lol << " <= " << kek << (lol <= kek ? " = True" : " = False") << endl;
-    break;
-  }
-
-  case 4:
-  {
-    cout << "\n//-- Summ Test --//\n\n";
-    Rational rr;
-    for (int i = 1; i < 30; i++)
-    {
-      Rational temp(1, i);
-      rr = rr + temp;
-      cout << rr << " = " << rr.get_value() << endl;
+  switch (test_n) {
+    case 0: {
+      Rational r1(-11205729, 14420), r2(3349, 14420), r3(11207689, 14420), r4(5439, 103);
+      cout << r1 << " = " << r1.normalize() << " = " << r1.normalize() << " = " << r1.denormalize() << endl;
+      cout << r2 << " = " << r2.normalize() << " = " << r2.normalize() << " = " << r2.denormalize() << endl;
+      cout << r3 << " = " << r3.normalize() << " = " << r3.normalize() << " = " << r3.denormalize() << endl;
+      cout << r4 << " = " << r4.normalize() << " = " << r4.normalize() << " = " << r4.denormalize() << endl;
+      break;
     }
-    break;
-  }
-  case 5: // shell sort
-  {
-    int count = 9;
-    Rational *arr[count] = {
-        new Rational(1, 2),
-        new Rational(12, 21),
-        new Rational(13, 22),
-        new Rational(14, 23),
-        new Rational(15, 24),
-        new Rational(0, 1),
+    
+    case 1: {
+      // тут тестируются:
+      // конструкторы, геттеры, сеттеры,
+      // вывод, конструктор копий, присваивания
+
+      Rational lol(1, 2), kek = {2, 3};
+      cout << "Rational 1 / 2: " << lol << endl;
+      cout << "Rational 2 / 3: " << kek << endl;
+
+      Rational temp(618);
+      cout << "Int 618 to Rational: " << temp << endl;
+
+      Rational test(42, 618);
+      cout << "42 / 618 = " << test << " -> ";
+
+      test.set(618, 42);
+      cout << test << " -> ";
+      cout << test.numerator() << " / "
+           << test.denominator() << " -> ";
+
+      test.numerator() = 6;
+      test.denominator() = 18;
+      cout << test << endl;
+
+      Rational test_copy(test);
+      cout << test << " = " << test.simplify()
+           << " => copy: " << test_copy << endl;
+
+      lol = kek = temp = test;
+      cout << lol << ", "
+           << kek << ", "
+           << temp << endl;
+      break;
+    }
+
+    case 2: {
+      // тут тестируются:
+      // ++ -- -n + += - -= * *= / /=
+
+      Rational lol(42, 618);
+      Rational kek(69, 420);
+      long long abc = 777;
+
+      cout << "++" << lol << " = " << ++lol << "\n";
+      cout << "--" << lol << " = " << --lol << "\n";
+      cout << "-" << lol << " = " << -lol << "\n";
+
+      cout << lol << " + " << kek << " = " << lol + kek << endl;
+      cout << lol << " + " << abc << " = " << lol + abc << endl;
+      cout << abc << " + " << lol << " = " << abc + lol << endl;
+
+      cout << lol << " - " << kek << " = " << lol - kek << endl;
+      cout << lol << " - " << abc << " = " << lol - abc << endl;
+      cout << abc << " - " << lol << " = " << abc - lol << endl;
+
+      cout << lol << " * " << kek << " = " << (lol * kek).normalize() << endl;
+      cout << lol << " * " << abc << " = " << (lol * abc).normalize() << endl;
+      cout << abc << " * " << lol << " = " << (abc * lol).normalize() << endl;
+
+      cout << lol << " / " << kek << " = " << lol / kek << endl;
+      cout << lol << " / " << abc << " = " << lol / abc << endl;
+      cout << abc << " / " << lol << " = " << abc / lol << endl;
+
+      lol = {42, 618};
+      kek = {69, 420};
+      abc = 777;
+      cout << lol << " += " << kek << " -> " << (lol += kek) << endl;
+      cout << lol << " += " << abc << " -> " << (lol += abc) << endl;
+
+      lol = {42, 618};
+      kek = {69, 420};
+      abc = 777;
+      cout << lol << " -= " << kek << " -> " << (lol -= kek) << endl;
+      cout << lol << " -= " << abc << " -> " << (lol -= abc) << endl;
+
+      lol = {42, 618};
+      kek = {69, 420};
+      abc = 777;
+      cout << lol << " *= " << kek << " -> " << (lol *= kek) << endl;
+      cout << lol << " *= " << abc << " -> " << (lol *= abc) << endl;
+
+      lol = {42, 618};
+      kek = {69, 420};
+      abc = 777;
+      cout << lol << " /= " << kek << " -> " << (lol /= kek) << endl;
+      cout << lol << " /= " << abc << " -> " << (lol /= abc) << endl;
+
+      break;
+    }
+
+    case 3: {
+      // тут тестируются:
+      // == != > >= < <=
+
+      Rational testA(1, 2);
+      Rational testB(1, 2);
+
+      if (testA == testB)
+        cout << "testA == testB" << endl;
+      if (testA == testA)
+        cout << "testA == testA" << endl;
+
+      Rational testNew(testA);
+      if (testA == testNew)
+        cout << "testA == testNew" << endl;
+
+      cout << ++testNew << ", " << testNew << endl;
+      if (testA == testNew)
+        cout << "testA == testNew" << endl;
+
+      cout << testA++ << ", " << testA << endl;
+      if (testA == testNew)
+        cout << "testA == testNew" << endl;
+
+      Rational temp(10, -20);
+      cout << temp << ", " << temp.simplify() << ", " << temp << endl;
+
+      Rational lol = {42, 618}, kek = {69, 420};
+      cout << lol << " > " << kek << (lol > kek ? " = True" : " = False") << endl;
+      cout << lol << " >= " << kek << (lol >= kek ? " = True" : " = False") << endl;
+      cout << lol << " < " << kek << (lol < kek ? " = True" : " = False") << endl;
+      cout << lol << " <= " << kek << (lol <= kek ? " = True" : " = False") << endl;
+      break;
+    }
+
+    case 4: {
+      cout << "\n//-- Summ Test --//\n\n";
+      Rational rr;
+      for (int i = 1; i < 30; i++) {
+        Rational temp(1, i);
+        rr = rr + temp;
+        cout << rr << " = " << rr.get_value() << endl;
+      }
+      break;
+    }
+
+    case 5: { // shell sort
+      int count = 9;
+      Rational *arr[count] = {
+        new Rational(1,   2),
+        new Rational(12,  21),
+        new Rational(13,  22),
+        new Rational(14,  23),
+        new Rational(15,  24),
+        new Rational(0,   1),
         new Rational(666, 6),
-        new Rational(16, 25),
-        new Rational(17, 26)};
-    cout << "original:\n";
-    for (int i = 0; i < count; i++)
-    {
-      cout << *(arr[i]) << endl;
+        new Rational(16,  25),
+        new Rational(17,  26)
+      };
+
+      cout << "original:\n";
+      for (int i = 0; i < count; i++) {
+        cout << *(arr[i]) << endl;
+      }
+
+      ShellSort(count, arr);
+      cout << "sorted:\n";
+      for (int i = 0; i < count; i++) {
+        cout << *(arr[i]) << endl;
+      }
     }
-    cout << "sorted:\n";
-    ShellSort(count, arr);
-    for (int i = 0; i < count; i++)
-    {
-      cout << *(arr[i]) << endl;
-    }
-  }
   }
 }
--- Variants/Lab_13/var_6/rational.cpp
+++ Variants/Lab_13/var_6/rational.cpp
@@ -3,67 +3,56 @@
 
 using namespace std;
 
-long long gcd(long long a, long long b)
-{
+long long gcd(long long a, long long b) {
   if (a == 0)
     return (b > 0) ? b : -b;
   return gcd(b % a, a);
 }
 
-long long lcm(long long a, long long b)
-{
+long long lcm(long long a, long long b) {
   return (a * b) / gcd(a, b);
 }
 
 Rational::Rational(long long numerator, long long denominator) : _integer(0),
                                                                  _numerator(denominator > 0 ? numerator : -numerator),
-                                                                 _denominator(denominator ? (denominator > 0 ? denominator : -denominator) : 1)
-{
+                                                                 _denominator(denominator ? (denominator > 0 ? denominator : -denominator) : 1) {
   assert(denominator != 0);
   this->simplify();
 }
 
 Rational::Rational(const Rational &temp, bool invert) : _integer(0),
                                                         _numerator(invert ? temp._denominator : temp._numerator),
-                                                        _denominator(invert ? temp._numerator : temp._denominator)
-{
+                                                        _denominator(invert ? temp._numerator : temp._denominator) {
   this->simplify();
 }
 
-Rational &Rational::set(long long numerator, long long denominator)
-{
+Rational &Rational::set(long long numerator, long long denominator) {
   _numerator = denominator > 0 ? numerator : -numerator;
   _denominator = denominator ? (denominator > 0 ? denominator : -denominator) : 1;
   return this->simplify();
 }
 
-long long &Rational::numerator()
-{
+long long &Rational::numerator() {
   return _numerator;
 }
 
-long long &Rational::denominator()
-{
+long long &Rational::denominator() {
   return _denominator;
 }
 
-long long &Rational::integer()
-{
+long long &Rational::integer() {
   return _integer;
 }
 
-double Rational::get_value()
-{
+double Rational::get_value() {
   return (double)_numerator / (double)_denominator;
 }
 
-Rational &Rational::simplify()
-{
+Rational &Rational::simplify() {
   long long factor = gcd(_numerator, _denominator);
   if (_denominator < 0)
     factor *= -1;
-  if (factor != 1)
-  {
+  if (factor != 1) {
     _numerator /= factor;
     _denominator /= factor;
   }
@@ -71,25 +60,20 @@ Rational &Rational::simplify()
   return *this;
 }
 
-Rational &Rational::normalize()
-{
+Rational &Rational::normalize() {
   if (!_integer)
     _integer = _numerator / _denominator;
   _numerator %= _denominator;
-  // if (_integer && _numerator < 0) _numerator *= -1;
   return *this;
 }
 
-Rational &Rational::denormalize()
-{
-  // if (_integer < 0 && _numerator > 0) _numerator *= -1;
+Rational &Rational::denormalize() {
   _numerator += _integer * _denominator;
   _integer = 0;
   return *this;
 }
 
-istream &operator>>(istream &in, Rational &temp)
-{
+istream &operator>>(istream &in, Rational &temp) {
   long long num, denom;
   in >> num;
   in >> denom;
@@ -97,14 +81,11 @@ istream &operator>>(istream &in, Rational &temp)
   return in;
 }
 
-ostream &operator<<(ostream &out, const Rational &r)
-{
+ostream &operator<<(ostream &out, const Rational &r) {
   long long num = r._numerator;
-  if (r._integer)
-  {
+  if (r._integer) {
     out << r._integer;
-    if (num)
-    {
+    if (num) {
       if (r._integer > 0)
         out << "+";
       if (r._integer < 0)
@@ -113,8 +94,7 @@ ostream &operator<<(ostream &out, const Rational &r)
         num *= -1;
     }
   }
-  if (r._numerator)
-  {
+  if (r._numerator) {
     out << "(" << num;
     if (r._denominator != 1)
       out << "/" << r._denominator;
@@ -125,8 +105,7 @@ ostream &operator<<(ostream &out, const Rational &r)
   return out;
 }
 
-Rational &Rational::operator=(const Rational &temp)
-{
+Rational &Rational::operator=(const Rational &temp) {
   // if (this == &temp) return *this;
   // (бесполезно когда нет динамической памяти)
   _numerator = temp._numerator;
@@ -134,56 +113,47 @@ Rational &Rational::operator=(const Rational &temp)
   return *this;
 }
 
-Rational &Rational::operator=(const long long &temp)
-{
+Rational &Rational::operator=(const long long &temp) {
   _numerator = temp;
   _denominator = 1;
   return *this;
 }
 
-bool operator==(const Rational &r1, const Rational &r2)
-{
+bool operator==(const Rational &r1, const Rational &r2) {
   return (r1._integer == r2._integer &&
           r1._numerator == r2._numerator &&
           r1._denominator == r2._denominator);
 }
 
-bool operator!=(const Rational &r1, const Rational &r2)
-{
+bool operator!=(const Rational &r1, const Rational &r2) {
   return !(r1 == r2);
 }
 
-const Rational Rational::operator+()
-{
+const Rational Rational::operator+() {
   return Rational(_numerator + _integer * _denominator, _denominator);
 }
 
-const Rational Rational::operator-()
-{
+const Rational Rational::operator-() {
   return Rational(-(_numerator + _integer * _denominator), _denominator);
 }
 
-Rational &Rational::operator++()
-{
+Rational &Rational::operator++() {
   _numerator += _denominator;
   return *this;
 };
 
-Rational &Rational::operator--()
-{
+Rational &Rational::operator--() {
   _numerator -= _denominator;
   return *this;
 };
 
-Rational Rational::operator++(int)
-{
+Rational Rational::operator++(int) {
   Rational temp(*this);
   ++(*this);
   return temp;
 };
 
-Rational Rational::operator--(int)
-{
+Rational Rational::operator--(int) {
   Rational temp(*this);
   --(*this);
   return temp;
@@ -191,8 +161,7 @@ Rational Rational::operator--(int)
 
 // + - * /
 
-Rational operator+(const Rational &r1, const Rational &r2)
-{
+Rational operator+(const Rational &r1, const Rational &r2) {
   Rational temp(
       (r1._numerator + r1._integer * r1._denominator) * r2._denominator +
           (r2._numerator + r2._integer * r2._denominator) * r1._denominator,
@@ -200,159 +169,133 @@ Rational operator+(const Rational &r1, const Rational &r2)
   return temp.simplify();
 };
 
-Rational operator+(const Rational &r, long long value)
-{
+Rational operator+(const Rational &r, long long value) {
   Rational temp(value);
   return temp + r;
 };
 
-Rational operator+(long long value, const Rational &r)
-{
+Rational operator+(long long value, const Rational &r) {
   return r + value;
 };
 
-Rational operator-(const Rational &r1, const Rational &r2)
-{
+Rational operator-(const Rational &r1, const Rational &r2) {
   Rational temp(-r2._numerator, r2._denominator);
   return r1 + temp;
 };
 
-Rational operator-(const Rational &r, long long value)
-{
+Rational operator-(const Rational &r, long long value) {
   Rational temp(value);
   return r - temp;
 };
 
-Rational operator-(long long value, const Rational &r)
-{
+Rational operator-(long long value, const Rational &r) {
   Rational temp(value);
   return temp - r;
 };
 
-Rational operator*(const Rational &r1, const Rational &r2)
-{
+Rational operator*(const Rational &r1, const Rational &r2) {
   Rational temp(
       (r1._numerator + r1._integer * r2._denominator) * (r2._numerator + r2._integer + r2._denominator),
       r1._denominator * r2._denominator);
   return temp.simplify();
 };
 
-Rational operator*(const Rational &r, long long value)
-{
+Rational operator*(const Rational &r, long long value) {
   Rational temp(value);
   return r * temp;
 };
 
-Rational operator*(long long value, const Rational &r)
-{
+Rational operator*(long long value, const Rational &r) {
   return r * value;
 };
 
-Rational operator/(const Rational &r1, const Rational &r2)
-{
+Rational operator/(const Rational &r1, const Rational &r2) {
   Rational temp(r2, INVERSE); // обратная дробь
   return r1 * temp;
 };
 
-Rational operator/(const Rational &r, long long value)
-{
+Rational operator/(const Rational &r, long long value) {
   Rational temp(value);
   return r / temp;
 };
 
-Rational operator/(long long value, const Rational &r)
-{
+Rational operator/(long long value, const Rational &r) {
   Rational temp(value);
   return temp / r;
 };
 
 // += -= *= /=
 
-Rational &operator+=(Rational &left, const Rational &right)
-{
+Rational &operator+=(Rational &left, const Rational &right) {
   left = left + right;
   return left;
 };
 
-Rational &operator+=(Rational &left, const long long &right)
-{
+Rational &operator+=(Rational &left, const long long &right) {
   left = left + right;
   return left;
 };
 
-Rational &operator-=(Rational &left, const Rational &right)
-{
+Rational &operator-=(Rational &left, const Rational &right) {
   left = left - right;
   return left;
 };
 
-Rational &operator-=(Rational &left, const long long &right)
-{
+Rational &operator-=(Rational &left, const long long &right) {
   left = left - right;
   return left;
 };
 
-Rational &operator*=(Rational &left, const Rational &right)
-{
+Rational &operator*=(Rational &left, const Rational &right) {
   left = left * right;
   return left;
 };
 
-Rational &operator*=(Rational &left, const long long &right)
-{
+Rational &operator*=(Rational &left, const long long &right) {
   left = left * right;
   return left;
 };
 
-Rational &operator/=(Rational &left, const Rational &right)
-{
+Rational &operator/=(Rational &left, const Rational &right) {
   left = left / right;
   return left;
 };
 
-Rational &operator/=(Rational &left, const long long &right)
-{
+Rational &operator/=(Rational &left, const long long &right) {
   left = left / right;
   return left;
 };
 
 // < <= > >=
 
-bool operator>(const Rational &r1, const Rational &r2)
-{
+bool operator>(const Rational &r1, const Rational &r2) {
   long long L = lcm(r1._denominator, r2._denominator);
   return (
       (r1._numerator + r1._integer * r1._denominator) * L / r1._denominator >
       (r2._numerator + r1._integer * r1._denominator) * L / r2._denominator);
 };
 
-bool operator<=(const Rational &r1, const Rational &r2)
-{
+bool operator<=(const Rational &r1, const Rational &r2) {
   return (r1 == r2 || r1 < r2);
 };
 
-bool operator<(const Rational &r1, const Rational &r2)
-{
+bool operator<(const Rational &r1, const Rational &r2) {
   return r2 > r1;
 };
 
-bool operator>=(const Rational &r1, const Rational &r2)
-{
+bool operator>=(const Rational &r1, const Rational &r2) {
   return (r1 == r2 || r1 > r2);
 };
 
 // сортировка Шелла
 
-void ShellSort(int n, Rational *mass[])
-{
+void ShellSort(int n, Rational *mass[]) {
   int i, j, step;
   Rational *tmp;
   for (step = n / 2; step > 0; step /= 2)
-    for (i = step; i < n; i++)
-    {
+    for (i = step; i < n; i++) {
       tmp = mass[i];
-      for (j = i; j >= step; j -= step)
-      {
+      for (j = i; j >= step; j -= step) {
         if (*tmp < *mass[j - step])
           mass[j] = mass[j - step];
         else
