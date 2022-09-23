252b6bd7e861b8fb91e2e9deb5f1c5f6fbeddd9b
Den <denis100703@gmail.com>
2022-09-24 01:09:24 +0300

    Lab 18 var 2 split to cpp & h files

--- Variants/Lab_18/var_2/main.cpp
+++ Variants/Lab_18/var_2/main.cpp
@@ -1,6 +1,7 @@
 // Реализовать базу данных о людях. Человек (вспомогательный класс Person)
-//описывается следующими полями: имя, фамилия, год рождения, пол, номер
-//паспорта. Рекомендуется перегрузить для класса Person оператор сравнения.
+// описывается следующими полями: имя, фамилия, год рождения, пол, номер
+// паспорта. Рекомендуется перегрузить для класса Person оператор сравнения
+
 // load(const std::string& filename)
 // save(const std::string& filename)
 // operator<< для вывода на экран
@@ -12,90 +13,9 @@
 #include <string>
 #include <vector>
 
+#include "database.h"
+
 using namespace std;
-
-class Person {
- private:
-  string name = "";
-  string surname = "";
-  int year = 0;
-  bool gender = 0;  // 1 woman, 0 man
-  int passport = 0;
-
- public:
-  Person()
-      : name("Rayan"),
-        surname("Gosling"),
-        year(1980),
-        gender(0),
-        passport(420069){};
-  Person(string name, string surname, int year, bool gender, int passport) {
-    this->name = name;
-    this->surname = surname;
-    this->year = year;
-    this->gender = gender;
-    this->passport = passport;
-  }
-
-  bool operator==(const Person &p1) {
-    return (this->name == p1.name) && (this->surname == p1.surname) &&
-           (this->year == p1.year) && (this->gender == p1.gender) &&
-           (this->passport == p1.passport);
-  }
-
-  friend ostream &operator<<(ostream &out, Person &p) {
-    out << p.name << " " << p.surname << " " << p.year << " " << p.gender << " " << p.passport << endl;
-    return out;
-  }
-};
-
-class Database {
- private:
-  vector<Person> db;
-
- public:
-  void load(const string &filename) {
-    ifstream fin(filename);
-    while (!fin.eof()) {
-      string name, surname;
-      int year, passport;
-      bool gender;
-      fin >> name >> surname >> year >> gender >> passport;
-      db.push_back(Person(name, surname, year, gender, passport));
-    }
-  }
-
-  void save(const string &filename) {
-    ofstream fout(filename);
-    for (int i = 0; i < db.size(); i++) {
-      fout << db[i];
-    }
-  }
-
-  void add(const Person &p) { db.push_back(p); }
-
-  friend ofstream &operator<<(ofstream &out, Database &d) {
-    for (int i = 0; i < d.db.size(); i++) {
-      out << d.db[i];
-    }
-    return out;
-  }
-
-  void erase(const Person &p) {
-    for (int i = 0; i < db.size(); i++) {
-      if (db[i] == p) {
-        db.erase(db.begin() + i);
-      }
-    }
-  }
-
-  friend ostream &operator<<(ostream &out, Database &d) {
-    for (int i = 0; i < d.db.size(); i++) {
-      out << d.db[i];
-    }
-    return out;
-  }
-};
 
 int main(int argc, char const *argv[]) {
   Database db;
@@ -107,15 +27,15 @@ int main(int argc, char const *argv[]) {
   db.add(Person("Tom", "Cruise", 1962, 0, 420074));
   db.add(Person("Jennifer", "Lawrence", 1990, 1, 420075));
   db.add(Person("Emma", "Watson", 1990, 1, 420076));
-  cout << db;
+  cout << db << endl;
 
   cout << "After erase" << endl;
   db.erase(Person("Bread", "Pitt", 1963, 0, 420071));
-  cout << endl << db;
+  cout << db << endl;
 
   cout << "After add" << endl;
   db.add(Person("Andrey", "Kruk", 1963, 1, 666));
-  cout << endl << db;
+  cout << db;
 
   db.save("db.txt");
   return 0;
