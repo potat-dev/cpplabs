8a419c4008c851c30d722e3707522f562bcc3e39
Den <denis100703@gmail.com>
2022-10-29 23:40:25 +0300

    Fixed bug with rotating non-square images

--- Practice/Lab_15/main.cpp
+++ Practice/Lab_15/main.cpp
@@ -1,3 +1,10 @@
+// Написать программу, формирующую по исходному BMP файлу новый BMP файл
+// повернутый по часовой стрелке на 90 градусов.
+
+// Указывать имена файлов исходных и результирующих файлов в командной строке
+// файлы после обработки заголовков читать целиком в динамически выделенную
+// память
+
 #include <cstdlib>
 #include <fstream>
 #include <iomanip>
@@ -5,10 +12,6 @@
 #include <string>
 
 #include "stdio.h"
-
-// указывать имена файлов исходных и результирующих файлов в командной строке
-// файлы после обработки заголовков читать целиком в динамически выделенную
-// память
 
 using namespace std;
 
@@ -34,23 +37,29 @@ struct __attribute__((__packed__)) BitMapInfoHeader {
   uint32_t Palette;    // (4) Число основных цветов
 };
 
-struct BGR {
+struct __attribute__((__packed__)) BGR {
   uint8_t Blue;   // (1) Синий
   uint8_t Green;  // (1) Зеленый
   uint8_t Red;    // (1) Красный
 };
 
+// rgb brightness calculation
+uint8_t rgb_brightness(BGR rgb) {
+  return (uint8_t)(0.299 * rgb.Red + 0.587 * rgb.Green + 0.114 * rgb.Blue);
+}
+
 void pretty_print(BGR **pixels, int h, int w) {
   for (int y = h - 1; y >= 0; y--) {
     for (int x = 0; x < w; x++)
-      printf("%02X %02X %02X | ", pixels[y][x].Red, pixels[y][x].Green,
-             pixels[y][x].Blue);
+      cout << (rgb_brightness(pixels[y][x]) > 128 ? " " : "#");
+      // printf("%02X %02X %02X | ", pixels[y][x].Red, pixels[y][x].Green,
+      //        pixels[y][x].Blue);
     printf("\n");
   }
 }
 
 int main() {
-  ifstream fin("kek.bmp", ios::binary);
+  ifstream fin("kek2.bmp", ios::binary);
   if (!fin.is_open()) {
     cout << "Error opening file" << endl;
     return 1;
@@ -85,9 +94,13 @@ int main() {
   BGR **pixels = new BGR *[image.Height];
   for (int i = 0; i < image.Height; i++) pixels[i] = new BGR[image.Width];
 
-  for (int y = 0; y < image.Height; y++)
-    for (int x = 0; x < image.Width; x++)
+  for (int y = 0; y < image.Height; y++) {
+    for (int x = 0; x < image.Width; x++) {
       fin.read((char *)&pixels[y][x], sizeof(BGR));
+    }
+    // skip padding
+    fin.seekg((4 - (image.Width * 3) % 4) % 4, ios::cur);
+  }
 
   pretty_print(pixels, image.Height, image.Width);
 
@@ -95,14 +108,23 @@ int main() {
   for (int i = 0; i < image.Width; i++) rotated[i] = new BGR[image.Height];
 
   // rotate 90 degrees ccw
-  for (int y = 0; y < image.Height; y++)
-    for (int x = 0; x < image.Width; x++)
-      rotated[x][image.Height - y - 1] = pixels[y][x];
+  BitMapFileHeader rotated_file = file;
+  BitMapInfoHeader rotated_image = image;
+  rotated_image.Width = image.Height;
+  rotated_image.Height = image.Width;
+
+  // rotate 90 degrees ccw
+  for (int y = 0; y < rotated_image.Height; y++)
+    for (int x = 0; x < rotated_image.Width; x++)
+      rotated[rotated_image.Height - y - 1][x] = pixels[x][y];
+
+  cout << "\n\n\nROTATED\n\n\n";
+  pretty_print(rotated, rotated_image.Height, rotated_image.Width);
 
   // rotate 90 degrees cw
   // for (int y = 0; y < image.Height; y++)
-  //   for (int x = 0; x < image.Width; x++)
-  //     rotated2[image.Width - x - 1][y] = pixels[y][x];
+  // for (int x = 0; x < image.Width; x++)
+  //   rotated2[image.Width - x - 1][y] = pixels[y][x];
 
   // write to file
   ofstream fout("output.bmp", ios::binary);
@@ -111,15 +133,26 @@ int main() {
     return 1;
   }
 
-  fout.write((char *)&file, sizeof(file));
-  fout.write((char *)&image, sizeof(image));
+  // image.Width = image.Height;
+  // image.Height = image.Width;
+
+  fout.write((char *)&rotated_file, sizeof(file));
+  cout << "Done!" << endl;
+  fout.write((char *)&rotated_image, sizeof(image));
+  cout << "Done!" << endl;
 
   // go to the start of the image data
-  fout.seekp(file.Offset, ios::beg);
+  fout.seekp(rotated_file.Offset, ios::beg);
+  cout << "Done!" << endl;
 
-  for (int y = 0; y < image.Width; y++)
-    for (int x = 0; x < image.Height; x++)
+  // write image data
+  for (int y = 0; y < rotated_image.Height; y++) {
+    for (int x = 0; x < rotated_image.Width; x++) {
       fout.write((char *)&rotated[y][x], sizeof(BGR));
+    }
+    // add padding
+    fout.seekp((4 - (rotated_image.Width * 3) % 4) % 4, ios::cur);
+  }
 
   cout << "Done!" << endl;
 }
